a45c8c4bf3fac4a9488f7e98eb4dcf13d94a892f
iwgx <wahabiputra@gmail.com>
2019-04-28 09:24:55 +0700

    Add react navigation

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import Home from './screens/Home';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +10,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Home />
       </View>
     );
   }
@@ -35,15 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
---
+++ screens/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Navbar
+        </Text>
+        <Text>
+          List of Current Journey
+        </Text>
+        <Text>
+          Bottom Bar
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Home;
